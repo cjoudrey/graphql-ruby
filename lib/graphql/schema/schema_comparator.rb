320c617b08dc2a98e649be7a3ac8485a2cd1176d
Christian Joudrey <cmallette@gmail.com>
2016-10-18 22:08:05 -0400

    wip

--- lib/graphql/schema/schema_comparator.rb
+++ lib/graphql/schema/schema_comparator.rb
@@ -8,6 +8,18 @@ module GraphQL
         TYPE_ADDED = :TYPE_ADDED,
         TYPE_KIND_CHANGED = :TYPE_KIND_CHANGED,
         TYPE_DESCRIPTION_CHANGED = :TYPE_DESCRIPTION_CHANGED,
+        ENUM_VALUE_REMOVED = :ENUM_VALUE_REMOVED,
+        ENUM_VALUE_ADDED = :ENUM_VALUE_ADDED,
+        ENUM_VALUE_DESCRIPTION_CHANGED = :ENUM_VALUE_DESCRIPTION_CHANGED,
+        ENUM_VALUE_DEPRECATED = :ENUM_VALUE_DEPRECATED,
+        UNION_MEMBER_REMOVED = :UNION_MEMBER_REMOVED,
+        UNION_MEMBER_ADDED = :UNION_MEMBER_ADDED,
+        DIRECTIVE_REMOVED = :DIRECTIVE_REMOVED,
+        DIRECTIVE_ADDED = :DIRECTIVE_ADDED,
+        DIRECTIVE_DESCRIPTION_CHANGED = :DIRECTIVE_DESCRIPTION_CHANGED,
+        DIRECTIVE_ARGUMENT_DESCRIPTION_CHANGED = :DIRECTIVE_ARGUMENT_DESCRIPTION_CHANGED,
+        DIRECTIVE_ARGUMENT_REMOVED = :DIRECTIVE_ARGUMENT_REMOVED,
+        DIRECTIVE_ARGUMENT_ADDED = :DIRECTIVE_ARGUMENT_ADDED,
       ]
 
       def compare(old_schema, new_schema)
@@ -43,7 +55,14 @@ module GraphQL
         def find_changes_in_types(old_type, new_type)
           changes = []
 
-          changes.push(*[]) # TODO
+          if old_type.class == new_type.class
+            case old_type
+            when EnumType
+              changes.push(*find_changes_in_enum_types(old_type, new_type))
+            when UnionType
+              changes.push(*find_changes_in_union_type(old_type, new_type))
+            end
+          end
 
           if old_type.description != new_type.description
             changes.push(type_description_changed(new_type))
@@ -57,7 +76,102 @@ module GraphQL
         end
 
         def find_changes_in_directives(old_schema, new_schema)
-          [] # TODO
+          old_directives = old_schema.directives.values.map(&:name)
+          new_directives = new_schema.directives.values.map(&:name)
+
+          removed = (old_directives - new_directives).map{ |directive| directive_removed(directive) }
+
+          added = (new_directives - old_directives).map{ |directive| directive_added(directive) }
+
+          changed = (old_directives & new_directives).map{ |directive|
+            changes = []
+
+            old_directive = old_schema.directives[directive]
+            new_directive = new_schema.directives[directive]
+
+            changes << directive_description_changed(directive) if old_directive.description != new_directive.description
+            changes.push(*find_changes_in_directive(old_directive, new_directive))
+
+            changes
+          }.flatten
+
+          removed + added + changed
+        end
+
+        def find_changes_in_directive(old_directive, new_directive)
+#      description = SchemaChange.DirectiveArgumentDescriptionChanged(newDir, _, _, _),
+#      default = SchemaChange.DirectiveArgumentDefaultChanged(newDir, _, _, _),
+#      typeChange = SchemaChange.DirectiveArgumentTypeChanged(newDir, _, _, _, _))
+          location_changes = [] #    val locationChanges = findInDirectiveLocations(oldDir, newDir)
+
+          field_changes = find_changes_in_args(
+            old_directive.arguments,
+            new_directive.arguments,
+            removed_method: lambda { |argument| directive_argument_removed(argument, new_directive) },
+            added_method: lambda { |argument, breaking_change| directive_argument_added(argument, breaking_change, new_directive) },
+            description_method: lambda { |argument| directive_argument_description_changed(argument, new_directive) },
+          )
+
+          location_changes + field_changes
+        end
+
+        def find_changes_in_args(old_arguments, new_arguments, removed_method:, added_method:, description_method:)
+          old = old_arguments.values.map(&:name)
+          new = new_arguments.values.map(&:name)
+
+          removed = (old - new).map(&removed_method)
+
+          added = (new - old).map{ |argument|
+            required_argument = new_arguments[argument].type.class == NonNullType
+            added_method.call(argument, required_argument)
+          }
+
+          changed = (old & new).map{ |argument|
+            old_argument = old_arguments[argument]
+            new_argument = new_arguments[argument]
+
+            changes = []
+
+            changes << description_method.call(argument) if old_argument.description != new_argument.description
+
+            changes
+          }.flatten
+
+          removed + added + changed
+        end
+
+        def find_changes_in_enum_types(old_type, new_type)
+          old_values = old_type.values.keys
+          new_values = new_type.values.keys
+
+          removed = (old_values - new_values).map{ |value| enum_value_removed(value, new_type) }
+
+          added = (new_values - old_values).map{ |value| enum_value_added(value, new_type) }
+
+          changed = (old_values & new_values).map{ |value|
+            old_value = old_type.values[value]
+            new_value = new_type.values[value]
+
+            changes = []
+
+            changes << enum_value_description_changed(value, new_type) if old_value.description != new_value.description
+            changes << enum_value_deprecated(value, new_type) if old_value.deprecation_reason != new_value.deprecation_reason
+
+            changes
+          }.flatten
+
+          removed + added + changed
+        end
+
+        def find_changes_in_union_type(old_type, new_type)
+          old_types = old_type.possible_types.map(&:name)
+          new_types = new_type.possible_types.map(&:name)
+
+          removed = (old_types - new_types).map{ |type| union_member_removed(type, new_type) }
+
+          added = (new_types - old_types).map{ |type| union_member_added(type, new_type) }
+
+          removed + added
         end
 
         def type_removed(type)
@@ -88,6 +202,102 @@ module GraphQL
           {
             type: TYPE_DESCRIPTION_CHANGED,
             description: "`#{type.name}` type description is changed",
+            breaking_change: false,
+          }
+        end
+
+        def enum_value_added(value, enum_type)
+          {
+            type: ENUM_VALUE_ADDED,
+            description: "Enum value `#{value}` was added to enum `#{enum_type.name}`",
+            breaking_change: false,
+          }
+        end
+
+        def enum_value_removed(value, enum_type)
+          {
+            type: ENUM_VALUE_REMOVED,
+            description: "Enum value `#{value}` was removed from enum `#{enum_type.name}`",
+            breaking_change: true,
+          }
+        end
+
+        def enum_value_description_changed(value, enum_type)
+          {
+            type: ENUM_VALUE_DESCRIPTION_CHANGED,
+            description: "`#{enum_type.name}.#{value}` description changed",
+            breaking_change: false,
+          }
+        end
+
+        def enum_value_deprecated(value, enum_type)
+          {
+            type: ENUM_VALUE_DEPRECATED,
+            description: "Enum value `#{value}` was deprecated in enum `#{enum_type.name}`",
+            breaking_change: false,
+          }
+        end
+
+        def union_member_removed(type, union_type)
+          {
+            type: UNION_MEMBER_REMOVED,
+            description: "`#{type}` type was removed from union `#{union_type.name}`",
+            breaking_change: true,
+          }
+        end
+
+        def union_member_added(type, union_type)
+          {
+            type: UNION_MEMBER_ADDED,
+            description: "`#{type}` type was added to union `#{union_type.name}`",
+            breaking_change: false,
+          }
+        end
+
+        def directive_added(directive)
+          {
+            type: DIRECTIVE_ADDED,
+            description: "`#{directive}` directive was added",
+            breaking_change: false,
+          }
+        end
+
+        def directive_removed(directive)
+          {
+            type: DIRECTIVE_REMOVED,
+            description: "`#{directive}` directive was removed",
+            breaking_change: true,
+          }
+        end
+
+        def directive_description_changed(directive)
+          {
+            type: DIRECTIVE_DESCRIPTION_CHANGED,
+            description: "`#{directive}` directive description is changed",
+            breaking_change: false,
+          }
+        end
+
+        def directive_argument_removed(argument, directive)
+          {
+            type: DIRECTIVE_ARGUMENT_REMOVED,
+            description: "Argument `#{argument}` was removed from `#{directive.name}` directive",
+            breaking_change: true,
+          }
+        end
+
+        def directive_argument_added(argument, breaking_change, directive)
+          {
+            type: DIRECTIVE_ARGUMENT_ADDED,
+            description: "Argument `#{argument}` was added to `#{directive.name}` directive",
+            breaking_change: breaking_change,
+          }
+        end
+
+        def directive_argument_description_changed(argument, directive)
+          {
+            type: DIRECTIVE_ARGUMENT_ADDED,
+            description: "`#{directive.name}(#{argument})` description is changed",
             breaking_change: false,
           }
         end
--- spec/graphql/schema/schema_comparator_spec.rb
+++ spec/graphql/schema/schema_comparator_spec.rb
@@ -89,4 +89,206 @@ describe GraphQL::Schema::SchemaComparator do
       breaking_change: false,
     }], changes
   end
+
+  it "detects changes in enum values" do
+    schema1 = <<-SCHEMA
+      enum Foo {
+        A, B, C
+      }
+    SCHEMA
+
+    schema2 = <<-SCHEMA
+      enum Foo {
+        B @deprecated(reason: "Should not be used anymore")
+        # The `B`
+        # value!
+        C, D
+      }
+    SCHEMA
+
+    changes = GraphQL::Schema::SchemaComparator.compare(schema(schema1), schema(schema2))
+
+    assert_equal 4, changes.length
+
+    assert_includes changes, {
+      type: GraphQL::Schema::SchemaComparator::ENUM_VALUE_REMOVED,
+      description: "Enum value `A` was removed from enum `Foo`",
+      breaking_change: true,
+    }
+
+    assert_includes changes, {
+      type: GraphQL::Schema::SchemaComparator::ENUM_VALUE_ADDED,
+      description: "Enum value `D` was added to enum `Foo`",
+      breaking_change: false,
+    }
+
+    assert_includes changes, {
+      type: GraphQL::Schema::SchemaComparator::ENUM_VALUE_DESCRIPTION_CHANGED,
+      description: "`Foo.C` description changed",
+      breaking_change: false,
+    }
+
+    assert_includes changes, {
+      type: GraphQL::Schema::SchemaComparator::ENUM_VALUE_DEPRECATED,
+      description: "Enum value `B` was deprecated in enum `Foo`",
+      breaking_change: false,
+    }
+  end
+
+  it "detects changes in unions" do
+    schema1 = <<-SCHEMA
+      type Foo {f: String}
+      type Bar {descr: String}
+      union Agg = Foo | Bar
+    SCHEMA
+
+    schema2 = <<-SCHEMA
+      type Bar {descr: String}
+      type Baz {descr: String}
+
+      # Hello
+      union Agg = Bar | Baz
+    SCHEMA
+
+    changes = GraphQL::Schema::SchemaComparator.compare(schema(schema1), schema(schema2))
+
+    assert_equal 5, changes.length
+
+    assert_includes changes, {
+      type: GraphQL::Schema::SchemaComparator::TYPE_ADDED,
+      description: "`Baz` type was added",
+      breaking_change: false,
+    }
+
+    assert_includes changes, {
+      type: GraphQL::Schema::SchemaComparator::TYPE_REMOVED,
+      description: "`Foo` type was removed",
+      breaking_change: true,
+    }
+
+    assert_includes changes, {
+      type: GraphQL::Schema::SchemaComparator::UNION_MEMBER_REMOVED,
+      description: "`Foo` type was removed from union `Agg`",
+      breaking_change: true,
+    }
+
+    assert_includes changes, {
+      type: GraphQL::Schema::SchemaComparator::UNION_MEMBER_ADDED,
+      description: "`Baz` type was added to union `Agg`",
+      breaking_change: false,
+    }
+
+    assert_includes changes, {
+      type: GraphQL::Schema::SchemaComparator::TYPE_DESCRIPTION_CHANGED,
+      description: "`Agg` type description is changed",
+      breaking_change: false,
+    }
+  end
+
+  it "detects changes in scalars" do
+    schema1 = <<-SCHEMA
+      scalar Date
+      scalar Locale
+    SCHEMA
+
+    schema2 = <<-SCHEMA
+      # This is locale
+      scalar Locale
+
+      # This is country
+      scalar Country
+    SCHEMA
+
+    changes = GraphQL::Schema::SchemaComparator.compare(schema(schema1), schema(schema2))
+
+    assert_equal 3, changes.length
+
+    assert_includes changes, {
+      type: GraphQL::Schema::SchemaComparator::TYPE_REMOVED,
+      description: "`Date` type was removed",
+      breaking_change: true,
+    }
+
+    assert_includes changes, {
+      type: GraphQL::Schema::SchemaComparator::TYPE_ADDED,
+      description: "`Country` type was added",
+      breaking_change: false,
+    }
+
+    assert_includes changes, {
+      type: GraphQL::Schema::SchemaComparator::TYPE_DESCRIPTION_CHANGED,
+      description: "`Locale` type description is changed",
+      breaking_change: false,
+    }
+  end
+
+  it "detects changes in directives" do
+    schema1 = <<-SCHEMA
+      directive @foo(a: String, b: Int!) on FIELD_DEFINITION | ENUM
+      directive @bar on FIELD_DEFINITION
+    SCHEMA
+
+    schema2 = <<-SCHEMA
+      # This is foo
+      directive @foo(
+        # first arg
+        a: String,
+        c: Int) on FIELD_DEFINITION | INPUT_OBJECT
+
+      # This is baz
+      directive @baz on FIELD_DEFINITION
+    SCHEMA
+
+    changes = GraphQL::Schema::SchemaComparator.compare(schema(schema1), schema(schema2))
+
+#    assert_equal 8, changes.length
+
+    assert_includes changes, {
+      type: GraphQL::Schema::SchemaComparator::DIRECTIVE_REMOVED,
+      description: "`bar` directive was removed",
+      breaking_change: true,
+    }
+
+    assert_includes changes, {
+      type: GraphQL::Schema::SchemaComparator::DIRECTIVE_ADDED,
+      description: "`baz` directive was added",
+      breaking_change: false,
+    }
+
+    assert_includes changes, {
+      type: GraphQL::Schema::SchemaComparator::DIRECTIVE_DESCRIPTION_CHANGED,
+      description: "`foo` directive description is changed",
+      breaking_change: false,
+    }
+
+    assert_includes changes, {
+      type: GraphQL::Schema::SchemaComparator::DIRECTIVE_ARGUMENT_DESCRIPTION_CHANGED,
+      description: "`foo(a)` description is changed",
+      breaking_change: false,
+    }
+
+    assert_includes changes, {
+      type: GraphQL::Schema::SchemaComparator::DIRECTIVE_ARGUMENT_REMOVED,
+      description: "Argument `b` was removed from `foo` directive",
+      breaking_change: true,
+    }
+
+    assert_includes changes, {
+      type: GraphQL::Schema::SchemaComparator::DIRECTIVE_LOCATION_ADDED,
+      description: "`InputObject` directive location added to `foo` directive",
+      breaking_change: false,
+    }
+
+    assert_includes changes, {
+      type: GraphQL::Schema::SchemaComparator::DIRECTIVE_LOCATION_REMOVED,
+      description: "`Enum` directive location removed from `foo` directive",
+      breaking_change: true,
+    }
+
+    assert_includes changes, {
+      type: GraphQL::Schema::SchemaComparator::DIRECTIVE_ARGUMENT_ADDED,
+      description: "Argument `c` was added to `foo` directive",
+      breaking_change: false,
+    }
+  end
 end
